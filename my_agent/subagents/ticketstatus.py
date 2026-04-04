3ff324ee9e59b535cb3026cee3f647f321139184
Jimson Sou <soukjimson@gmail.com>
2026-04-04 15:47:16 -0700

    TXTS-124 Modified instructions to take data and respond to user

--- my_agent/subagents/ticketstatus.py
+++ my_agent/subagents/ticketstatus.py
@@ -1,10 +1,13 @@
 from google.adk.agents import LlmAgent
 from ..config import MODEL, TICKETSTATUS_INSTRUCTION
+from .tools.ticket_search import search_tickets
+
 
 ticketstatus_agent = LlmAgent(
     model=MODEL,
     name="TicketStatusAgent",
     description="Finds and returns ticket status updates.",
     instruction=TICKETSTATUS_INSTRUCTION,
+    tools=[search_tickets],
     output_key="ticketstatus",
 )
