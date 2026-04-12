aad424779a07e6c18c372233fbc2a421418243dc
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2026-04-12 07:39:20 -0700

    TXTS-116/Developed instructions for the end-conversation agent

--- my_agent/subagents/end_conversation.py
+++ my_agent/subagents/end_conversation.py
@@ -6,5 +6,8 @@ end_conversation = LlmAgent(
     model=MODEL,
     name="EndConversationAgent",
     description="Gracefully end conversation.",
-    instruction="""""",
+    instruction="""
+    If the user doesn't have any more questions, make sure to do these things: 
+    - End the conversation politely by confirming. For example, say something like: 'Great!' or 'You're welcome!' if they say thank you. DON'T DOUBLE CONFIRM.
+    - Finish by adding 'Thank you for contacting 311 service center.'.""",
 )
