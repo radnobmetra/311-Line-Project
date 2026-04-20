aca67484a7998c35623ca8d3b2b5e4e583c4c01f
Kyra Hodgson <201279317+khodgson2@users.noreply.github.com>
2026-04-19 22:46:48 -0700

    txts-154: Update check_input_len to check that most of user's input is letters, numbers, spaces

--- my_agent/subagents/overseer.py
+++ my_agent/subagents/overseer.py
@@ -42,7 +42,8 @@ def check_input_len(user_input: str) -> bool:
     # Progresses through each character in the user input.
     for char in user_input:
         # ord() returns the Unicode code point of the character.
-        if ord(char) < 128:
+        code = ord(char)
+        if (code > 96 and code < 123) or (code > 64 and code <91) or (code > 47 and code < 58) or (code == 32):
             counter = counter + 1
     # Returns True if the user's input is at least 70% ASCII characters.
     if counter / len(user_input) >= 0.7:
--- my_agent/subagents/tools/validateinput.py
+++ my_agent/subagents/tools/validateinput.py
@@ -34,19 +34,20 @@ def user_mal_input(user_input: str) -> bool:
 
 def check_input_len(user_input: str) -> bool:
     """
-    Counts the number of chars in the user's input and ensures that 70% of the input is made up of ASCII characters.
+    Counts the number of chars in the user's input and ensures that 70% of the input is made up of letters.
 
     Args:
         user_input (str): The user input to check.
 
     Returns:
-        bool: True if 70% or more of the characters in the user's input are ASCII, False otherwise.
+        bool: True if 70% or more of the characters in the user's input are letters, False otherwise.
     """
     counter = 0
     # Progresses through each character in the user input.
     for char in user_input:
         # ord() returns the Unicode code point of the character.
-        if ord(char) < 128:
+        code = ord(char)
+        if (code > 96 and code < 123) or (code > 64 and code <91) or (code > 47 and code < 58) or (code == 32):
             counter = counter + 1
     # Returns True if the user's input is at least 70% ASCII characters.
     if counter / len(user_input) >= 0.7:
