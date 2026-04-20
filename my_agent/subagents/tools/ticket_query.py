f22baccfcbe89c5e98b8bea7587def9dc7a18054
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2026-04-20 09:26:20 -0700

    TXTS-144/Created 2 functions: one that takes the ticket number and validates if it matches the number format. The second one convert the retrieved Epoch time from ArcGIS to local time.

--- my_agent/subagents/tools/ticket_query.py
+++ my_agent/subagents/tools/ticket_query.py
@@ -1,4 +1,61 @@
 import requests
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import re
+
+
+def validate_ticket_num_format(ticket_number):
+    """
+    Ticket number must exactly match this format (6 digits, a dash, then 7 digits).
+    For example, ticket number should be like this "123456-1234567"
+    If a dash is missing, it will be added.
+
+    Returns:
+    True and the ticket number
+    or
+    False and empty string if ticket number isn't valid
+    """
+    validated_number = ticket_number
+
+    # First check: checks if exact match.
+    pattern = r"\d{6}-\d{7}"
+    match = re.fullmatch(pattern, ticket_number)
+
+    # Second check: checks if a dash is missing.
+    if match is None:
+        pattern = r"\d{13}"
+        match = re.fullmatch(pattern, ticket_number)
+
+        # Add a dash if it's missing.
+        if match is not None:
+            validated_number = ticket_number[:6] + "-" + ticket_number[6:]
+        else:
+            validated_number = ""
+
+    is_correct_format = match != None
+
+    return is_correct_format, validated_number
+
+
+def convert_Epoch_to_localtime(epoch_time):
+    """
+    Time retrieved from the ArcGIS database is stored as UNIX Epoch time.
+    This function converts epoch time to local time.
+    For example, 1776148140000 will be converted to 04/13/2026 11:29 PM
+    """
+    formatted_time = ""
+
+    # If the inputted time is null/empty, the function will return empty string.
+    if epoch_time != None:
+        # Epoch time is in milliseconds. Divide it by 1000 to convert it to seconds.
+        epoch_time = epoch_time / 1000.0
+
+        datetime_obj = datetime.fromtimestamp(
+            epoch_time, tz=ZoneInfo("America/Los_Angeles")
+        )
+        formatted_time = datetime_obj.strftime("%m/%d/%Y %I:%M %p")
+
+    return formatted_time
 
 
 def ArcGIS_get_ticket(ticket_number):
