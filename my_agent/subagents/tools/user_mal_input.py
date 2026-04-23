3117f367d435ddba3dfbf148eacafe7e3bca13bf
radnobmetra <artem.s.bondar2004@gmail.com>
2026-04-23 00:23:48 -0700

    See branch name.

--- my_agent/subagents/tools/user_mal_input.py
+++ my_agent/subagents/tools/user_mal_input.py
@@ -1,3 +1,5 @@
+import json
+
 def user_mal_input(user_input: str) -> bool:
     """
     Checks if the user's input is malicious.
@@ -8,7 +10,10 @@ def user_mal_input(user_input: str) -> bool:
     Returns:
         bool: True if the user's input is found to be malicious, False otherwise.
     """
-    mal_keywords = ["firewall", "phish", "hack", "bypass", "sql inject", "ddos"]
+    # Set list "mal_keywords" to the contents of the emergency_cases.json file
+    with open('data.json', 'r') as mal_keywords:
+        mal_keywords = json.load("emergency_cases.json")
+
     # Checks each keyword in the list against the user input.
     for keyword in mal_keywords:
         if keyword in user_input.lower():
