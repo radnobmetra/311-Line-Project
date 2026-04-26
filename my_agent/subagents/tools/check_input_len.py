cdffdbdf848c75b7100f8b0d8a4bf0eac69e341c
Kyra Hodgson <201279317+khodgson2@users.noreply.github.com>
2026-04-26 13:00:29 -0700

    change how input is verified in check_input_len

--- my_agent/subagents/tools/check_input_len.py
+++ my_agent/subagents/tools/check_input_len.py
@@ -1,19 +1,17 @@
 def check_input_len(user_input: str) -> bool:
     """
-    Counts the number of chars in the user's input and ensures that 70% of the input is made up of ASCII characters.
+    Counts the number of chars in the user's input and ensures that 70% of the input is made up of alphanumeric characters.
 
     Args:
         user_input (str): The user input to check.
 
     Returns:
-        bool: True if 70% or more of the characters in the user's input are ASCII, False otherwise.
+        bool: True if 70% or more of the characters in the user's input are alphanumeric chars, False otherwise.
     """
     counter = 0
     # Progresses through each character in the user input.
     for char in user_input:
-        # ord() returns the Unicode code point of the character.
-        code = ord(char)
-        if (code > 96 and code < 123) or (code > 64 and code <91) or (code > 47 and code < 58) or (code == 32):
+        if (char.isalnum()):
             counter = counter + 1
     # Returns True if the user's input is at least 70% alphanumeric characters.
     if counter / len(user_input) >= 0.7:
