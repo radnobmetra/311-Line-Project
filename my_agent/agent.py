4a0e6b8963efc72db513410514e7ce45a27a4913
Bee <lordsbleorji@gmail.com>
2026-04-26 13:48:28 -0700

    added a 30 minute timer where if user is inactive it will log the messages of the user
    edited agent to a timer will always be running
    in the background
    
    check_input_len now begins this inactivity timer,
    when user sends message
    
    timertracker keeps track of all user messages
    
    timerloop is the background loop that reads
    useronline.json to see if user is still active

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,3 +1,9 @@
+import threading
 from .subagents import overseer_agent
+from timerloop import run_loop 
+
+#enables timer in background; for inactivity
+timerloop_thread = threading.Thread(target=run_loop, daemon=True)
+timerloop_thread.start()
 
 root_agent = overseer_agent
--- my_agent/subagents/tools/check_input_len.py
+++ my_agent/subagents/tools/check_input_len.py
@@ -1,21 +1,28 @@
+from timertracker import update_user_status
+
 def check_input_len(user_input: str) -> bool:
     """
-    Counts the number of chars in the user's input and ensures that 70% of the input is made up of ASCII characters.
+    Counts the number of chars in the user's input and ensures that 70% of the input is made up of alphanumeric characters.
 
     Args:
         user_input (str): The user input to check.
 
     Returns:
-        bool: True if 70% or more of the characters in the user's input are ASCII, False otherwise.
+        bool: True if 70% or more of the characters in the user's input are alphanumeric chars, False otherwise.
     """
+    # logs activity first when ran
+    update_user_status("user", user_input) 
+    
+    # character check logic
     counter = 0
     # Progresses through each character in the user input.
     for char in user_input:
-        # checking if each character is ascii.
-        if char.isascii():
+        if char.isalnum():
             counter += 1
-    # Returns True if the user's input is at least 70% ASCII characters.
+            
+    # Returns True if the user's input is at least 70% alphanumeric characters.
     if counter / len(user_input) >= 0.7:
         return True
+        
     # Returns False if otherwise.
     return False
