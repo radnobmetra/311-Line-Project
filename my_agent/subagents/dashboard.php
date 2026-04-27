c3765de5bd121ca17e4f442f2e36cc6812903d7d
radnobmetra <artem.s.bondar2004@gmail.com>
2026-04-26 23:42:29 -0700

    Finalized Admin Dashboard

--- my_agent/subagents/dashboard.php
+++ my_agent/subagents/dashboard.php
@@ -1,8 +1,7 @@
 <?php
-  echo "Hello!";
-  $conn = mysqli_connect("localhost","root","","session.db");
+  $conn = new SQLite3('../.adk/session.db');
   $sql = "Select * from sessions";
-  $result = mysqli_query($conn, $sql);
+  $result = $conn->query($sql);
 ?>
 
 <!doctype html>
@@ -10,11 +9,58 @@
   <head>
     <meta charset="utf-8" />
     <title>Admin Dashboard</title>
+
+  <style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #121212; 
+    color: #e0e0e0;
+    margin: 20px;
+  }
+  
+  h1 {
+    color: #ffffff;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    background-color: #1e1e1e;
+  }
+
+  th, td {
+    border: 1px solid #333;
+    padding: 12px 15px;
+    text-align: left;
+  }
+
+  td:nth-child(2) {
+    word-break: break-all;
+  }
+  
+  th:nth-child(3), td:nth-child(3) {
+    width: 35%;
+    word-break: break-word;
+  }
+
+  th {
+    background-color: #2c2c2c;
+    color: #4da6ff;
+    font-weight: bold;
+    font-size: 1.25em;
+  }
+
+  tr:nth-child(even) {
+    background-color: #252525;
+  }
+</style>
+
   </head>
   <body>
     <h1>Admin Dashboard</h1>
 
-    <table>
+   <table>
       <tr>
         <th>App Name</th>
         <th>ID</th>
@@ -23,22 +69,20 @@
         <th>Update Time</th>
       </tr>
 
-        <!-- <?php
-        //while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-
+      <?php
+      while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+      ?>
         <tr>
-          <td>
-            <?php 
-            // echo $row[''];
-            ?>
-          </td>
+          <td><?php echo $row['app_name']; ?></td>
+          <td><?php echo $row['id']; ?></td>
+          <td><?php echo $row['state']; ?></td>
+          <td><?php echo $row['create_time']; ?></td>
+          <td><?php echo $row['update_time']; ?></td>
         </tr>
 
-        <?php
-        //}
-
-      ?> -->
+      <?php
+      }
+      ?> 
 
     </table>
 
