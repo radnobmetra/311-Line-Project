617fcc1c6f0613c1232d6ff6a9636eb6bd658b0b
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2026-09-12 11:59:52 -0700

    TXTS-182-profile-details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,10 +7,17 @@ import { Login } from './login/login';
 import { guardian } from './guardian'; 
 
 export const routes: Routes = [
+    // { path: 'login', component: Login }, //login page
+    // { path: 'overview', component: Overview, canActivate: [guardian] }, //guadian ensures valid entry
+    // { path: 'settings', component: Settings, canActivate: [guardian] },
+    // { path: 'analytics', component: Analytics, canActivate: [guardian] },
+    // { path: 'requests', component: Requests, canActivate: [guardian] },
+    // { path: '', redirectTo: '/login', pathMatch: 'full' } //redirct
+
     { path: 'login', component: Login }, //login page
-    { path: 'overview', component: Overview, canActivate: [guardian] }, //guadian ensures valid entry
-    { path: 'settings', component: Settings, canActivate: [guardian] },
-    { path: 'analytics', component: Analytics, canActivate: [guardian] },
-    { path: 'requests', component: Requests, canActivate: [guardian] },
-    { path: '', redirectTo: '/login', pathMatch: 'full' } //redirct
+    { path: 'overview', component: Overview,  }, //guadian ensures valid entry
+    { path: 'settings', component: Settings,  },
+    { path: 'analytics', component: Analytics,  },
+    { path: 'requests', component: Requests,  },
+    { path: '', redirectTo: '/overview', pathMatch: 'full' } //redirct
 ];
