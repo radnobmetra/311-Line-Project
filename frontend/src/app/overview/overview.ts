787b8703dbdbe8b6964eff0ba6f0109794c484da
Bee <lordsbleorji@gmail.com>
2026-09-11 17:52:55 -0700

    made a collaspisble sidebar with different pages for navigation.

---
+++ frontend/src/app/overview/overview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-overview',
+  styleUrl: './overview.css',
+  templateUrl: './overview.html',
+})
+export class Overview {}
---
+++ frontend/src/app/settings/settings.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-settings',
+  styleUrl: './settings.css',
+  templateUrl: './settings.html',
+})
+export class Settings {}
