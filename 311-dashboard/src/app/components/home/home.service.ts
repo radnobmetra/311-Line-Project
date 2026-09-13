a216740e43d20c538ef31051c3857ec528e09a38
Jimson Sou <soukjimson@gmail.com>
2026-09-13 02:44:43 -0700

    Remove redundent components (graph)

--- 311-dashboard/src/app/components/home/home.service.ts
+++ 311-dashboard/src/app/components/home/home.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 
-export class GraphService {
+export class HomeService {
     API_URL = 'http://localhost:3000/ticket_counts';
 
     constructor(private _httpclient:HttpClient) { 
--- 311-dashboard/src/app/components/home/home.ts
+++ 311-dashboard/src/app/components/home/home.ts
@@ -1,13 +1,89 @@
 import { Component, OnInit} from '@angular/core';
-import { Graph } from './graph/graph';
+import { Chart, registerables } from 'chart.js';
+import { HomeService } from './home.service';
+
+Chart.register(...registerables);
 
 @Component({
-  imports: [ Graph],
+  imports: [],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {
+export class Home implements OnInit {
+
+  title = 'ticketData';
+
+  data: any;
+  dataStatus: any[] = [];
+  dataCount: any[] =[];
+  constructor(private _homeService: HomeService) {
+
+  }
+
+
+  ngOnInit() {
+    this._homeService.showdata().subscribe(res => {
+
+      this.data = res;
+
+      if (this.data != null) {
+        for (let i = 0; i < this.data.length; i++) {
+          this.dataStatus.push(this.data[i].status);
+          this.dataCount.push(this.data[i].count);
+        }
+      }
+      console.log(this.dataCount)
+      this.showGraph(this.dataStatus, this.dataCount);
+
+    });
+
+  }
 
   
+  showGraph(dataStatus: any, dataCount: any) {
+    new Chart("ticketChart", {
+      type: 'bar',
+      data: {
+        labels: dataStatus, /*This is label of each bar */
+        datasets: [{
+          label: 'Ticket Count',  /*This is label of overall graph */
+          data: dataCount,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    new Chart("ticketChart2", {
+      type: 'bar',
+      data: {
+        labels: dataStatus, /*This is label of each bar */
+        datasets: [{
+          label: 'Ticket Count',  /*This is label of overall graph */
+          data: dataCount,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+
 }
